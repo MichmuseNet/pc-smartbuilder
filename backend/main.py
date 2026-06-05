afea6dfc9645854657980a97bffd00059559f1b4
Andrea Michelle Olivas Rivera <andreaolivas@Andreas-MacBook-Pro-3.local>
2026-06-05 12:29:15 -0600

    agregando vercel url a cors

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://pc-smartbuilder.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
